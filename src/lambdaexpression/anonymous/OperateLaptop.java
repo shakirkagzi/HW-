4edf3629e6143aaf5dab13b38fc08aa77e29f3dd
shakirkagzi <shakirkagzi@gmail.com>
2020-09-06 19:55:17 -0400

    first home work.

--- src/lambdaexpression/anonymous/OperateLaptop.java
+++ src/lambdaexpression/anonymous/OperateLaptop.java
@@ -4,16 +4,24 @@ public class OperateLaptop {
 
     public static void main(String[] args) {
         //Please implement anonymous class for interface laptop
-        Laptop laptop = null;
+        Laptop laptop = new Laptop() {
+            @Override
+            public void ramSize(int memorySize) {
+                System.out.println("Memory Size: " + memorySize);
+            }
 
-
-
+            @Override
+            public void monitorSize(int screenSize) {
+                System.out.println("Screen SIze: " + screenSize);
+            }
+        };
 
         laptop.monitorSize(16);
         laptop.monitorSize(15);
 
-        //please implement anonymous function for Phone FunctionalInterface
-
+        //please implement anonymous function for Phone interface
+        Phone phone = n->System.out.println("Cellular Network: " + n);
+        phone.cellularNetwork("T-Mobile");
 
     }
 }
